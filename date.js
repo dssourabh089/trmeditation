a4007da75003d5343b380f46fe6f039eb27ca6f9
dssourabh089 <106088973+dssourabh089@users.noreply.github.com>
2022-06-13 11:43:02 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let registrationNo = prompt( "enter your registration no" );
+if ( ( registrationNo >= "000194" ) && ( registrationNo <= "002566" ) ){
+    alert( "The date for collecting your registration is 13/06/2022" )
+} 
+     else if ( ( registrationNo >="002588") && ( registrationNo <= "004449") ) {
+    alert ("The date for collecting your registration is 15/06/2022")
+}
+    else if ( ( registrationNo >="004454") && ( registrationNo <= "007452") ) {
+    alert ("The date for collecting your registration is 16/06/2022")
+}
+else if ( ( registrationNo >="007492") && ( registrationNo <= "010327") ) {
+    alert ("The date for collecting your registration is 17/06/2022")
+}
+else if ( ( registrationNo >="010388") && ( registrationNo <= "020570") ) {
+    alert ("The date for collecting your registration is 18/06/2022")
+} 
